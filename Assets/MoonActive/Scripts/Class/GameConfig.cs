a031d05f297285cc1a7a324373855a7e6f8be94e
Timuçin Topal <timucintopal@gmail.com>
2023-09-23 16:35:24 +0300

    Initial commit

---
+++ Assets/MoonActive/Scripts/Class/GameConfig.cs
@@ -0,0 +1,11 @@
+namespace MoonActive.Scripts
+{
+    public class GameConfig
+    {
+        public int ButtonAmount;
+        public int PointPerStep;
+        public int Duration;
+        public bool IsRepeating = false;
+        public float SpeedMultiplier = 1;
+    }
+}
